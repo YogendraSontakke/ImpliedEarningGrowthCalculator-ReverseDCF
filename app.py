675e820ab64a2995ef0dabd9ece6ebb106b89c75
SONTAKKE Yogendra <YJE@dsone.3ds.com>
2024-07-18 10:31:07 -0600

    Fixed Bug

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Needed for flash messages
 
-def calculate_implied_growth_rate(current_earnings, discount_rate, terminal_multiple, current_valuation, years=5):
+def calculate_implied_growth_rate(net_profit, discount_rate, terminal_multiple, market_capitalization, years=5):
     def calculate_dcf(growth_rate):
-        cash_flows = [current_earnings * ((1 + growth_rate) ** t) for t in range(1, years + 1)]
+        cash_flows = [net_profit * ((1 + growth_rate) ** t) for t in range(1, years + 1)]
         terminal_value = cash_flows[-1] * terminal_multiple
         
         dcf = sum(cf / ((1 + discount_rate) ** t) for t, cf in enumerate(cash_flows, start=1))
@@ -20,7 +20,7 @@ def calculate_implied_growth_rate(current_earnings, discount_rate, terminal_mult
         mid = (low + high) / 2
         dcf = calculate_dcf(mid)
         
-        if dcf < current_valuation:
+        if dcf < market_capitalization:
             low = mid
         else:
             high = mid
@@ -32,41 +32,41 @@ def index():
     result = None
     error = None
     form_data = {
-        'current_earnings': '',
+        'net_profit': '',
         'discount_rate': '',
         'years': '',
         'terminal_multiple': '',
-        'current_valuation': ''
+        'market_capitalization': ''
     }
 
     if request.method == 'POST':
         try:
-            current_earnings = float(request.form['current_earnings'])
+            net_profit = float(request.form['net_profit'])
             discount_rate = float(request.form['discount_rate']) / 100.0
             years = int(request.form['years'])
             terminal_multiple = float(request.form['terminal_multiple'])
-            current_valuation = float(request.form['current_valuation'])
+            market_capitalization = float(request.form['market_capitalization'])
             
             form_data = {
-                'current_earnings': current_earnings,
+                'net_profit': net_profit,
                 'discount_rate': discount_rate * 100,
                 'years': years,
                 'terminal_multiple': terminal_multiple,
-                'current_valuation': current_valuation
+                'market_capitalization': market_capitalization
             }
 
-            implied_growth_rate = calculate_implied_growth_rate(current_earnings, discount_rate, terminal_multiple, current_valuation, years)
+            implied_growth_rate = calculate_implied_growth_rate(net_profit, discount_rate, terminal_multiple, market_capitalization, years)
             
-            future_earnings = current_earnings * ((1 + implied_growth_rate) ** years)
+            future_earnings = net_profit * ((1 + implied_growth_rate) ** years)
             market_cap = future_earnings * terminal_multiple
-            absolute_return = 100 * ((market_cap - current_valuation) / current_valuation)
+            absolute_return = 100 * ((market_cap - market_capitalization) / market_capitalization)
             
             result = {
-                'current_earnings': current_earnings,
+                'net_profit': net_profit,
                 'discount_rate': discount_rate,
                 'years': years,
                 'terminal_multiple': terminal_multiple,
-                'current_valuation': current_valuation,
+                'market_capitalization': market_capitalization,
                 'implied_growth_rate': implied_growth_rate,
                 'future_earnings': future_earnings,
                 'market_cap': market_cap,
